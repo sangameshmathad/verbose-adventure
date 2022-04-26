e3698925e0e179addd439c52e91a580c040abc1b
Sangamesh Mathad <102652901+sangameshmathad@users.noreply.github.com>
2022-04-26 13:11:58 +0530

    Create reverseString.js

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+ let str = "Friends";
+let myArray = str.split('');
+console.log(myArray);
+myArray.reverse();
+ let ans= myArray.join('');
+console.log(ans);
+console.log(str.split('').reverse().join(''));
